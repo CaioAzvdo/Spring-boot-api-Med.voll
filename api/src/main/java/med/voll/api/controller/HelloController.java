d96d5d8cc167934ce760dc76932ccf86065e85f3
CaioAzvdo <caioazevedodourado8@gmail.com>
2023-05-31 14:17:49 -0300

    Modifiquei a classe controller HelloController

--- api/src/main/java/med/voll/api/controller/HelloController.java
+++ api/src/main/java/med/voll/api/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String olaMundo(){
-        return "Estou fazendo uma mudança aq pae, ve ai se funfou";
+        return "Estou fazendo uma mudança aq pae, olha ai se funfou";
     }
 }
